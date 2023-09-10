b0579300893f408361a17da3f44844c1b323e9b9
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2023-09-10 23:09:00 +0200

    init image post

--- controllers/ControllerRealization/controllerRealization.js
+++ controllers/ControllerRealization/controllerRealization.js
@@ -4,11 +4,13 @@ const controllerRealization = express.Router();
 
 const getRealizationsList = require('./routes/getRealizationsList')
 const postRealization = require('./routes/postRealization')
+const postRealizationImage = require('./routes/postRealizationImage')
 
 
 
 controllerRealization.use('/', getRealizationsList)
 controllerRealization.use('/', postRealization)
+controllerRealization.use('/', postRealizationImage)
 
 
 module.exports = controllerRealization;
---
+++ controllers/ControllerRealization/routes/postRealizationImage.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../../db');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post("/:id/image/:position", upload.single('image'), async(req, res, next) =>{
+    var errors = []
+    const { buffer } = req.file;
+    console.log(buffer)
+    // if(!req.body.image){
+    //     errors.push("Nie dodano zdjęcia")
+    // }
+    // if(!req.body.position){
+    //     errors.push("Nie podano pozycji zdjęcia")
+    // }
+    // if(!req.body.imageCount){
+    //     errors.push("Nie podano liczby zdjęć")
+    // }
+    
+    // add weblink
+    // if (errors.length) {
+    //     res.status(400).json({"error": errors.join(",")})
+    //     return
+    // }
+    let created_at = Date()
+
+    // let data={
+    //     title:req.body.title,
+    //     description:req.body.description,
+    //     // imageCount:req.body.imageCount,
+    //     created_at: created_at
+    // }
+    // let lastInsertOfferId
+    // try{
+    //     // // let param = [req.params.id]
+
+    //     // let body =[data.title, data.description, data.created_at,]
+    //     // let returnOfferRow = await db.query("INSERT INTO realizations(title, description, created_at) VALUES ($1,$2,$3)RETURNING id",body)
+        
+    //     // // console.log(experiences)
+    //     // lastInsertOfferId = returnOfferRow.rows[0].id;
+
+        
+
+        
+
+    // }catch (error) {
+    //     res.status(400).json({"error": error.message})
+        
+    //     return;
+    // }
+    res.json({
+        "message": 0
+    })
+    
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+module.exports = router;
