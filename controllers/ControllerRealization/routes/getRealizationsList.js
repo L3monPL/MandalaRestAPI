b7027bdcdf2d693f78927551454a9319b35ffc5b
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2023-09-11 15:10:21 +0200

    change realizations list

--- controllers/ControllerRealization/routes/getRealizationsList.js
+++ controllers/ControllerRealization/routes/getRealizationsList.js
@@ -9,7 +9,32 @@ router.get("", async (req, res) => {
       // query the database to retrieve all data from the "users" table
       const { rows } = await db.query('SELECT * FROM realizations');
       // send the retrieved data as the response
-      res.send(rows);
+      var list = new Array()
+
+
+      for (let index = 0; index < rows.length; index++) {
+        
+        let listImage = []
+
+        for (let index = 0; index < rows.length; index++) {
+          let imageObject = await db.query('SELECT id, realizationId, position, description, created_at FROM images WHERE realizationId = $1', [rows[index].id]);
+          listImage.push(imageObject.rows[0])
+        }
+
+        // console.log(imageObject)
+
+        list.push({
+          id: rows[index].id,
+          title: rows[index].title,
+          description: rows[index].description,
+          images: listImage,
+          created_at: rows[index].created_at
+        })
+      }
+
+      console.log(list)
+
+      res.send(list);
     } catch (error) {
       console.error(error);
       res.status(500).send('Server error');
